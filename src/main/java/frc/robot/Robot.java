77735ee369a26ed1e9811b1034f17a286d3e3032
Colin Wong <cwongmath@gmail.com>
2022-11-17 17:25:59 -0600

    Change indentation

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,63 +5,63 @@ import frc.robot.components.*;
 
 
 public class Robot extends TimedRobot {
-  private static enum Mode{
-    DISABLED, TELEOP
-  }
+    private static enum Mode{
+        DISABLED, TELEOP
+    }
 
-  private Mode mode = Mode.DISABLED;
+    private Mode mode = Mode.DISABLED;
 
-  private Drivebase drivebase;
-  private Intake intake;
-  private Shooter shooter;
+    private Drivebase drivebase;
+    private Intake intake;
+    private Shooter shooter;
 
-  private Teleop teleop;
+    private Teleop teleop;
 
-  @Override
-  public void robotInit(){
-    teleop = new Teleop();
+    @Override
+    public void robotInit(){
+        teleop = new Teleop();
 
-    drivebase = new Drivebase();
-    intake = new Intake();
-    shooter = new Shooter();
-  }
-  
-  @Override
-  public void robotPeriodic() {
-  }
+        drivebase = new Drivebase();
+        intake = new Intake();
+        shooter = new Shooter();
+    }
+    
+    @Override
+    public void robotPeriodic() {
+    }
 
-  @Override
-  public void teleopInit() {
-    mode = Mode.TELEOP;
-  }
+    @Override
+    public void teleopInit() {
+        mode = Mode.TELEOP;
+    }
 
-  @Override
-  public void teleopPeriodic() {
-    teleop.periodic(this);
-  }
+    @Override
+    public void teleopPeriodic() {
+        teleop.periodic(this);
+    }
 
-  @Override
-  public void disabledInit(){
-    mode = Mode.DISABLED;
-  }
+    @Override
+    public void disabledInit(){
+        mode = Mode.DISABLED;
+    }
 
-  public Mode getMode(){
-    return mode;
-  }
+    public Mode getMode(){
+        return mode;
+    }
 
-  public Teleop getTeleop(){
-    return teleop;
-  }
-  
-  public Drivebase getDrivebase(){
-     return drivebase;
-  }
+    public Teleop getTeleop(){
+        return teleop;
+    }
+    
+    public Drivebase getDrivebase(){
+         return drivebase;
+    }
 
-  public Shooter getShooter(){
-    return shooter;
-  }
+    public Shooter getShooter(){
+        return shooter;
+    }
 
-  public Intake getIntake(){
-    return intake;
-  }
+    public Intake getIntake(){
+        return intake;
+    }
 }
