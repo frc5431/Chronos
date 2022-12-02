284ecdf30ad42ab4e29f8fc6287785cb61ca4456
Natalie <64873273+DoubleSnapps@users.noreply.github.com>
2022-12-01 19:26:09 -0600

    hehe3

--- src/main/java/frc/robot/components/Shooter.java
+++ src/main/java/frc/robot/components/Shooter.java
@@ -12,7 +12,7 @@ public class Shooter{
         rightShooter = new WPI_TalonSRX(Constants.RIGHT_SHOOTER_ID);
         rightShooter.setInverted(Constants.RIGHT_SHOOTER_INVERTED);
         rightShooter.setNeutralMode(NeutralMode.Coast);
-
+        
         leftShooter = new WPI_TalonSRX(Constants.LEFT_SHOOTER_ID);
         leftShooter.setInverted(Constants.LEFT_SHOOTER_INVERTED);
         leftShooter.setNeutralMode(NeutralMode.Coast);
