f89aeaa30555500c9dca68ac009f69342364e14d
Natalie <64873273+DoubleSnapps@users.noreply.github.com>
2022-12-01 19:26:02 -0600

    hehe2

--- src/main/java/frc/robot/systems/Systems.java
+++ src/main/java/frc/robot/systems/Systems.java
@@ -3,10 +3,12 @@ package frc.robot.systems;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import frc.robot.systems.subsystems.GoodIntake;
+import frc.robot.systems.subsystems.GoodShooter;
 
 import frc.robot.Constants;
 import frc.robot.Robot;
 import frc.robot.components.Intake;
+import frc.robot.components.Shooter;
 
 public class Systems{
     
@@ -14,21 +16,27 @@ public class Systems{
 
     private WPI_TalonSRX intakeMotor;
 
+    private GoodShooter shooter;
+
+    private WPI_TalonSRX shooterMotorR, shooterMotorL;
+    
+
     public Systems(){
-
         intakeMotor = new WPI_TalonSRX(Constants.INTAKE_ID);
-
         intake = new GoodIntake(intakeMotor);
+        shooterMotorL = new WPI_TalonSRX(Constants.LEFT_SHOOTER_ID);
+        shooterMotorR = new WPI_TalonSRX(Constants.RIGHT_SHOOTER_ID);
+        shooter = new GoodShooter(shooterMotorL, shooterMotorR);
 
     }
-
-
+    
+    
     public GoodIntake gIntake(){
-
         return intake;
-
     }
-
+    public GoodShooter gShooter(){
+        return shooter;
+    }
 
 
 
